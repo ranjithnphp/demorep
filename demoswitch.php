b0ec55120deb2d6320d1418d53da0356fe537663
ranjithnphp <ranjithranjithpvt@gmail.com>
2023-09-18 17:57:03 +0530

    demorep edited

--- demoswitch.php
+++ demoswitch.php
@@ -1,30 +1,13 @@
 <?php
 
-$no1 = 3;echo"<br>";
-$no2 = -2;
 
-echo abs($no2);
-echo "<br>";
-echo ($no1/$no2);
-echo "<br>";
-echo abs($no1/$no2);
-echo "<br>";
-echo pi();
-echo "<br>";
-echo sqrt(64);
-echo "<br>";
-echo abs(round($no1/$no2));
-echo "<br>";
-echo rand(1,100);
-echo "<br>";
-echo round(-10.1);echo"<br>";
-echo round(10.6);echo"<br>";
-echo ceil(10.4);echo"<br>";
-echo ceil(10.8);echo"<br>";
-echo ceil(-9.8);echo"<br>";
-echo floor(10.9);echo"<br>";
-echo floor(9.2);echo"<br>";
-echo floor(-10.6);echo"<br>";
-
+$data = "blue"; 
+switch($data){
+	case "blue":echo "blue";
+	break;
+	case "red":echo "red";
+	break;
+	case "green":echo "green";
+}
 
 ?>
